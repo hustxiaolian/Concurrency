a100fe97c328f70212a23e3c9ef29441958dd13a
xiaolian <2504033134@qq.com>
2018-07-25 21:55:31 +0800

    今晚开了个并发的头子，以后按照thinking_in_java的步骤慢慢日常推进，争取这两周完事。今晚学习了并发的意义和使用场景，了解了协作式多线程。学习了定义线程的基本方法来运行多线程的基本方法。

---
+++ src/com/xiaolianhust/concurrency/thinkingInjava/E1_ThreatTest.java
@@ -0,0 +1,29 @@
+package com.xiaolianhust.concurrency.thinkingInjava;
+
+public class E1_ThreatTest implements Runnable{
+	public static int taskCount;
+	public final int id = taskCount++;
+	
+	public E1_ThreatTest() {
+		System.out.println("E1_ThreatTest "+ id + " start...");
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(id + ": info 1");
+		Thread.yield();
+		System.out.println(id + ": info 2");
+		Thread.yield();
+		System.out.println(id + ": info 3");
+		Thread.yield();
+		
+		System.out.println("Done!");
+	}
+	
+	public static void main(String[] args) {
+		for(int i = 0;i < 10;++i) {
+			new Thread(new E1_ThreatTest()).start();
+		}
+	}
+	
+}	
---
+++ src/com/xiaolianhust/concurrency/thinkingInjava/LiftOff.java
@@ -0,0 +1,36 @@
+package com.xiaolianhust.concurrency.thinkingInjava;
+
+public class LiftOff implements Runnable {
+	public int countDown = 10;
+	public static int taskCount = 0;
+	public final int taskId = taskCount++;
+	
+	public LiftOff(int countDown) {
+		super();
+		this.countDown = countDown;
+	}
+
+	public LiftOff() {}
+
+	@Override
+	public void run() {
+		while(countDown-- > 0) {
+			System.out.println(status());
+			Thread.yield();
+		}
+	}
+
+	private String status() {
+		return "#" + taskId + "(" + (countDown > 0 ? countDown:"LiftOff") + ").";
+	}
+	
+	public static void main(String[] args) {
+		//Thread t = new Thread(new LiftOff());
+		//t.start();
+		
+		for(int i = 0;i < 5;++i) {
+			new Thread(new LiftOff()).start();
+		}
+	}
+
+}
